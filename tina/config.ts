539fa65e954324183c7bedfca3551cf79c1f7157
Simon Hischier <simon_hischier@hotmail.com>
2024-02-10 17:21:37 +0100

    reinstalled tina

--- tina/config.ts
+++ tina/config.ts
@@ -1,107 +1,111 @@
-import { CATEGORIES } from '../src/data/categories.ts'
-import { defineConfig } from 'tinacms'
+import { CATEGORIES } from '../src/data/categories.ts';
+import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = 'main'
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
 
 export default defineConfig({
-	branch,
-	clientId: 'ef88b371-5a16-4aca-a62a-ed13c0cb8b0f',
-	token: 'cfa41d0d946c5372c5d4b5dbe89d0b4ca3937ffe',
-	build: {
-		outputFolder: 'admin',
-		publicFolder: 'public'
-	},
-	media: {
-		tina: {
-			mediaRoot: '/src/assets/images',
-			publicFolder: ''
-		}
-	},
-	schema: {
-		collections: [
-			{
-				name: 'post',
-				label: 'Blog Post',
-				path: 'src/content/blog',
-				format: 'mdx',
-				fields: [
-					{
-						type: 'image',
-						label: 'Cover Image',
-						required: true,
-						name: 'heroImage',
-						description: 'The image used for the cover of the post'
-					},
+  branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "/src/assets/images",
+      publicFolder: "",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: 'post',
+        label: 'Blog Post',
+        path: 'src/content/blog',
+        format: 'mdx',
+        fields: [
+          {
+            type: 'image',
+            label: 'Cover Image',
+            required: true,
+            name: 'heroImage',
+            description: 'The image used for the cover of the post'
+          },
 
-					{
-						type: 'string',
-						required: true,
-						name: 'category',
-						label: 'Category',
-						description: 'Select an category for this post',
-						options: [...CATEGORIES]
-					},
-					{
-						type: 'string',
-						label: 'description',
-						required: true,
-						name: 'description',
-						description: 'A short description of the post'
-					},
-					{
-						type: 'datetime',
-						name: 'pubDate',
-						label: 'Publication Date',
-						required: true
-					},
-					{
-						name: 'draft',
-						label: 'Draft',
-						type: 'boolean',
-						description: 'If this is checked the post will not be published'
-					},
-					{
-						type: 'string',
-						name: 'tags',
-						required: true,
-						label: 'Tags',
-						description: 'Tags for this post',
-						list: true,
-						ui: {
-							component: 'tags'
-						}
-					},
-					{
-						type: 'string',
-						name: 'title',
-						label: 'Title',
-						isTitle: true,
-						required: true
-					},
-					{
-						type: 'rich-text',
-						label: 'Body',
-						name: 'SButton',
-						isBody: true,
-						templates: [
-							// Custom Components
-							{
-								label: 'SButton',
-								name: 'SButton',
-								fields: [
-									{
-										type: 'rich-text',
-										label: 'SButton',
-										name: 'children',
-										isBody: true
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		]
-	}
-})
+          {
+            type: 'string',
+            required: true,
+            name: 'category',
+            label: 'Category',
+            description: 'Select an category for this post',
+            options: [...CATEGORIES]
+          },
+          {
+            type: 'string',
+            label: 'description',
+            required: true,
+            name: 'description',
+            description: 'A short description of the post'
+          },
+          {
+            type: 'datetime',
+            name: 'pubDate',
+            label: 'Publication Date',
+            required: true
+          },
+          {
+            name: 'draft',
+            label: 'Draft',
+            type: 'boolean',
+            description: 'If this is checked the post will not be published'
+          },
+          {
+            type: 'string',
+            name: 'tags',
+            required: true,
+            label: 'Tags',
+            description: 'Tags for this post',
+            list: true,
+            ui: {
+              component: 'tags'
+            }
+          },
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true
+          },
+          {
+            type: 'rich-text',
+            label: 'Body',
+            name: 'SButton',
+            isBody: true,
+            templates: [
+              // Custom Components
+              {
+                label: 'SButton',
+                name: 'SButton',
+                fields: [
+                  {
+                    type: 'rich-text',
+                    label: 'SButton',
+                    name: 'children',
+                    isBody: true
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
