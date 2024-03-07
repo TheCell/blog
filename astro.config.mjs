916702e0da1a541b7e4b50cac71ad380d2d871ae
Simon Hischier <simon_hischier@hotmail.com>
2024-03-07 23:57:26 +0100

    fixed code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,27 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
+// import { transformerNotationHighlight, transformerNotationFocus } from "@shikijs/transformers";
+import { transformerNotationHighlight, transformerNotationFocus, transformerNotationErrorLevel } from "shikiji-transformers";
 
 export default defineConfig({
 	site: 'https://newblog.thecell.eu/',
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind()
+	],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
+		syntaxHighlight: "shiki",
 		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
+			themes: {
+				light: 'github-light',
+				dark: 'github-dark-dimmed',
+			  },
+			wrap: true,	
+			transformers: [transformerNotationHighlight(), transformerNotationFocus(), transformerNotationErrorLevel()]
 		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
-})
+	}
+});
