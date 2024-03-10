c3d437f6a086a0f6d8124006bef5f8fef237867f
Simon Hischier <simon_hischier@hotmail.com>
2024-03-10 23:20:12 +0100

    added style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-// import { transformerNotationHighlight, transformerNotationFocus, transformerNotationErrorLevel } from "@shikijs/transformers";
-import { transformerNotationHighlight, transformerNotationFocus, transformerNotationErrorLevel } from "shikiji-transformers";
+// import { transformerNotationHighlight, transformerNotationFocus, transformerNotationErrorLevel, transformerNotationDiff } from "@shikijs/transformers";
+import { transformerNotationHighlight, transformerNotationFocus, transformerNotationErrorLevel, transformerNotationDiff } from "shikiji-transformers";
 
 export default defineConfig({
 	site: 'https://newblog.thecell.eu/',
@@ -23,7 +23,7 @@ export default defineConfig({
 				dark: 'github-dark-dimmed',
 			  },
 			wrap: true,	
-			transformers: [transformerNotationHighlight(), transformerNotationFocus(), transformerNotationErrorLevel()]
+			transformers: [transformerNotationHighlight(), transformerNotationFocus(), transformerNotationErrorLevel(), transformerNotationDiff()]
 		}
 	}
 });
